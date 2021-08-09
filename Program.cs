58815aff8eb33416e203590d8deae0dd037a8a4d
abi magpatoc <abimgptc@gmail.com>
2021-08-09 10:59:32 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DSTALGO_Lactao_n_Magpatoc
+{
+    class Program
+    {
+        public void Customer()
+        {
+            Console.WriteLine("What are you here for?");
+            Console.WriteLine("\t[1]-Book an appointment" + "\n\t[2]-View appointment schedules" +
+                "\n\t[3]-Request an Salon Staff");
+            Console.Write("\nEnter your choice: ");
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
