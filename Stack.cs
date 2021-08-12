6f8d28d88113f2be444274f413f679d2248c49fc
rlactao <raevictoria.lactao@benilde.edu.ph>
2021-08-12 10:50:24 +0800

    trying stack

---
+++ Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSTALGO_Lactao_n_Magpatoc
+{
+    class Stack
+    {
+        private object[] array;
+        private int top;
+
+        public Stack()
+        {
+            array = new object[5];
+            top = -1;
+                
+        }
+
+        public void Push(object item)
+        {
+            if(top < array.Length-1)
+            {
+                top++;
+                array[top] = item;
+            }
+        }
+    }
+}
