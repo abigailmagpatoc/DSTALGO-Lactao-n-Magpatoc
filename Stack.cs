a10b20857d624426c2369ac8df724ccd5d6194fd
rlactao <raevictoria.lactao@benilde.edu.ph>
2021-08-13 14:16:28 +0800

    adding stack for undo?

--- Stack.cs
+++ Stack.cs
@@ -8,23 +8,73 @@ namespace DSTALGO_Lactao_n_Magpatoc
 {
     class Stack
     {
-        private string[] array;
+        //for undo button? -> allow customer user to undo/pop recent appointment
+
+        private object[] array;
         private int top;
 
         public Stack()
         {
-            array = new string[5];
+            array = new object[5];
             top = -1;
                 
         }
 
-        public void Push(string item)
+        public void Push(object item)
         {
             if(top < array.Length-1)
             {
                 top++;
                 array[top] = item;
             }
+            else
+            {
+                throw new Exception("Stack Overflow");
+            }
         }
+
+        public object Pop()
+        {
+            if(top > -1)
+            {
+                object item = array[top];
+                top--;
+                return item;
+            }
+            else
+            {
+                throw new Exception("Stack Empty");
+            }
+        }
+
+
+        public object Peek()
+        {
+            if (top > -1)
+            {
+                object item = array[top];
+                return item;
+            }
+            else
+            {
+                throw new Exception("Stack Empty");
+            }
+        }
+
+        public void Clear()
+        {
+            top = -1;
+        }
+
+        public void ViewStack()
+        {
+            for (int i = top; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+
+
     }
 }
